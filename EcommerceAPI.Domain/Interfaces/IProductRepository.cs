f9eb64d8f6c0bc65f6f5bac97fc875d8e59a82b6
shakana0 <shakanajamal3@gmail.com>
2025-12-01 13:41:20 +0100

    feat: add interface for product repository

---
+++ EcommerceAPI.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿
+namespace EcommerceAPI.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> AddAsync(Product product);
+
+        Task<Product?> GetByIdAsync(int id);
+        Task<IEnumerable<Product>> GetAllAsync();
+
+        Task UpdateAsync(Product product);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}
