99b4a66f429d3367e64c6db5a2d05d2871ef8970
shakana0 <shakanajamal3@gmail.com>
2025-12-01 13:40:55 +0100

    reafctor: update access fields

--- EcommerceAPI.Domain/Entities/Category.cs
+++ EcommerceAPI.Domain/Entities/Category.cs
@@ -4,15 +4,15 @@ namespace EcommerceAPI.Domain;
 
 public class Category
 {
-    public int Id {get; private set;}
+    public int Id {get; set;}
     [Required]
     [MaxLength(100)]
-    public string Name { get; private set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
 
     [MaxLength(500)]
-    public string Description { get; private set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
 
-    protected Category() { }
+    public Category() { }
 
     public Category(string name, string description, int? parentCategoryId = null)
     {
--- EcommerceAPI.Domain/Entities/Product.cs
+++ EcommerceAPI.Domain/Entities/Product.cs
@@ -4,20 +4,20 @@ namespace EcommerceAPI.Domain;
 
 public class Product
 {
-    public int Id {get; private set;}
+    public int Id {get; set;}
   
     [Required]
     [MaxLength(100)]
-    public string Name { get; private set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
     [MaxLength(500)]
-    public string Description { get; private set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     [Required]
-    public decimal Price { get; private set; }
-    public int StockQuantity {get; private set;}
-    public int CategoryId {get; private set;}
-    public Category Category {get; private set;} = null!;
+    public decimal Price { get; set; }
+    public int StockQuantity {get; set;}
+    public int CategoryId {get; set;}
+    public Category Category {get; set;} = null!;
 
-    protected Product() { }
+    public Product() { }
 
     public Product(string name, string description, decimal price, int stockQuantity, int categoryId)
     {
