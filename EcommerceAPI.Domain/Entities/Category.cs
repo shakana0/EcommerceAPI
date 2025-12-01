8e0762c03615fb3f67e7a335305a354b88e391cb
shakana0 <shakanajamal3@gmail.com>
2025-12-01 10:16:43 +0100

    feat: add data annotation and constructor

--- EcommerceAPI.Domain/Entities/Category.cs
+++ EcommerceAPI.Domain/Entities/Category.cs
@@ -3,6 +3,19 @@ namespace EcommerceAPI.Domain;
 public class Category
 {
     public int Id {get; private set;}
-    public string Name {get; private set;} = string.Empty;
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; private set; } = string.Empty;
+
+    [MaxLength(500)]
     public string Description { get; private set; } = string.Empty;
+
+    protected Category() { }
+
+    public Category(string name, string description, int? parentCategoryId = null)
+    {
+        Name = name;
+        Description = description;
+        ParentCategoryId = parentCategoryId;
+    }
 }
