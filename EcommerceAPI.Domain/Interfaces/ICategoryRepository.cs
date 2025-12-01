5898eacdc602ec886003d694a0560a88c3d6766b
shakana0 <shakanajamal3@gmail.com>
2025-12-01 13:41:30 +0100

    feat: add interface for category repository

---
+++ EcommerceAPI.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿
+namespace EcommerceAPI.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> AddAsync(Category category);
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category?> GetByIdAsync(int id);
+        Task UpdateAsync(Category category);
+        Task<bool> DeleteAsync(int id);
+    }
+}
