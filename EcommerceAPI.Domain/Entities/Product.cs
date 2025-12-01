e6d6098e610e18afac6e4293b7ecef174f7ad735
shakana0 <shakanajamal3@gmail.com>
2025-12-01 10:14:43 +0100

    feat: att product entity

---
+++ EcommerceAPI.Domain/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace EcommerceAPI.Domain;
+
+public class Product
+{
+    public int Id {get; private set;}
+  
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; private set; } = string.Empty;
+    [MaxLength(500)]
+    public string Description { get; private set; } = string.Empty;
+    [Required]
+    public decimal Price { get; private set; }
+    public int StockQuantity {get; private set;}
+    public int CategoryId {get; private set;}
+    public Category Category {get; private set;} = null!;
+
+    protected Product() { }
+
+    public Product(string name, string description, decimal price, int stockQuantity, int categoryId)
+    {
+        Name = name;
+        Description = description;
+        Price = price;
+        StockQuantity = stockQuantity;
+        CategoryId = categoryId;
+    }
+}
