f93587db4805f0b86c2136897017735659464da6
shakana0 <shakanajamal3@gmail.com>
2025-12-06 15:12:48 +0100

    refactor: add GetById endpoint

--- EcommerceAPI.WebAPI/Controllers/ProductControllers.cs
+++ EcommerceAPI.WebAPI/Controllers/ProductControllers.cs
@@ -1,7 +1,6 @@
-﻿using EcommerceAPI.Domain;
-using EcommerceAPI.Domain.Interfaces;
-using EcommerceAPI.WebAPI.Dtos;
-using EcommerceAPI.WebAPI.DTOs;
+﻿using EcommerceAPI.Application.Products.Dtos;
+using EcommerceAPI.Application.Products.Queries.GetProductById;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -11,94 +10,24 @@ namespace EcommerceAPI.WebAPI.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductRepository _repository;
+        private readonly IMediator _mediator;
 
-        public ProductsController(IProductRepository repository)
+        public ProductsController(IMediator mediator)
         {
-            _repository = repository;
+            _mediator = mediator;
         }
-
-
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<ProductResponseDto>>> GetAll()
-        {
-            var products = await _repository.GetAllAsync();
-
-            var dtos = products.Select(p => new ProductResponseDto
-            {
-                Id = p.Id,
-                Name = p.Name,
-                Description = p.Description,
-                Price = p.Price,
-                StockQuantity = p.StockQuantity,
-                CategoryId = p.CategoryId,
-                CategoryName = p.Category?.Name,
-                CategoryDescription = p.Category?.Description
-            });
-
-            return Ok(dtos);
-        }
-
-
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<ProductResponseDto>> GetById(int id)
+        public async Task<ActionResult<ProductDto>> GetById(int id)
         {
-            var product = await _repository.GetByIdAsync(id);
+            var product = await _mediator.Send(new GetProductByIdQuery(id));
+
             if (product == null)
-                return NotFound();
+                return NotFound($"No Product found for Id: {id}");
 
-            var dto = new ProductResponseDto
-            {
-                Id = product.Id,
-                Name = product.Name,
-                Description = product.Description,
-                Price = product.Price,
-                StockQuantity = product.StockQuantity,
-                CategoryId = product.CategoryId,
-                CategoryName = product.Category?.Name,
-                CategoryDescription = product.Category?.Description
-            };
-
-            return Ok(dto);
+            return Ok(product);
         }
 
 
-        [HttpPost]
-        public async Task<ActionResult<Product>> Create(ProductRequestDto dto)
-        {
-            var product = new Product
-            {
-                Name = dto.Name,
-                Description = dto.Description,
-                Price = dto.Price,
-                StockQuantity = dto.StockQuantity,
-                CategoryId = dto.CategoryId
-            };
-
-            var created = await _repository.AddAsync(product);
-            return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
-        }
-
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Update(int id, Product product)
-        {
-            if (id != product.Id)
-                return BadRequest();
-
-            await _repository.UpdateAsync(product);
-            return NoContent();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var deleted = await _repository.DeleteAsync(id);
-            if (!deleted)
-                return NotFound();
-
-            return NoContent();
-        }
     }
 }
