c04ec8af91faa59a6ec02ebd80335cceeb41749b
shakana0 <shakanajamal3@gmail.com>
2025-12-06 16:23:30 +0100

    refactor: implment encapsulation

--- EcommerceAPI.Domain/Entities/Product.cs
+++ EcommerceAPI.Domain/Entities/Product.cs
@@ -4,18 +4,23 @@ namespace EcommerceAPI.Domain.Entities;
 
 public class Product
 {
-    public int Id {get; set;}
-  
+    public int Id { get; private set; }
+
     [Required]
     [MaxLength(100)]
-    public string Name { get; set; } = string.Empty;
+    public string Name { get; private set; } = string.Empty;
+
     [MaxLength(500)]
-    public string Description { get; set; } = string.Empty;
+    public string Description { get; private set; } = string.Empty;
+
     [Required]
-    public decimal Price { get; set; }
-    public int StockQuantity {get; set;}
-    public int CategoryId {get; set;}
-    public Category Category {get; set;} = null!;
+    public decimal Price { get; private set; }
+
+    public int StockQuantity { get; private set; }
+
+    public int CategoryId { get; private set; }
+
+    public Category Category { get; private set; } = null!;
 
     public Product() { }
 
@@ -26,5 +31,23 @@ public class Product
         Price = price;
         StockQuantity = stockQuantity;
         CategoryId = categoryId;
+    }
+
+    public void UpdateDetails(string? name, string? description, decimal? price, int? stockQuantity, int? categoryId)
+    {
+        if (!string.IsNullOrWhiteSpace(name))
+            Name = name;
+
+        if (!string.IsNullOrWhiteSpace(description))
+            Description = description;
+
+        if (price.HasValue && price.Value > 0)
+            Price = price.Value;
+
+        if (stockQuantity.HasValue && stockQuantity.Value >= 0)
+            StockQuantity = stockQuantity.Value;
+
+        if (categoryId.HasValue && categoryId.Value > 0)
+            CategoryId = categoryId.Value;
     }
 }
