20f31a4721599e08ff99df2d8bb3cac9c6bbc8f6
shakana0 <shakanajamal3@gmail.com>
2025-12-06 14:31:14 +0100

    feat: add CreateCategoryHandler

---
+++ EcommerceAPI.Application/Categories/Commands/CreateCategory/CreateCategoryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using EcommerceAPI.Application.Categories.Dtos;
+using EcommerceAPI.Domain.Entities;
+using EcommerceAPI.Domain.Interfaces;
+using MediatR;
+
+namespace EcommerceAPI.Application.Categories.Commands.CreateCategory
+{
+    public class CreateCategoryHandler : IRequestHandler<CreateCategoryCommand, CategoryDto>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CreateCategoryHandler(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CategoryDto> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var category = _mapper.Map<Category>(request);
+
+            var created = await _categoryRepository.AddAsync(category, cancellationToken);
+
+            return _mapper.Map<CategoryDto>(created);
+        }
+    }
+}
