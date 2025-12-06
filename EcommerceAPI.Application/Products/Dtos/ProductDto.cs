cfd094148f9cab3d0ad023068b54992980c8dfb2
shakana0 <shakanajamal3@gmail.com>
2025-12-06 15:47:35 +0100

    reafctor: update dto so it includes catgeory fields

--- EcommerceAPI.Application/Products/Dtos/ProductDto.cs
+++ EcommerceAPI.Application/Products/Dtos/ProductDto.cs
@@ -1,10 +1,15 @@
-﻿namespace EcommerceAPI.Application.Products.Dtos
+﻿using EcommerceAPI.Application.Categories.Dtos;
+
+namespace EcommerceAPI.Application.Products.Dtos
 {
     public record ProductDto(
-        string Name,
-        string Description,
-        decimal Price,
-        int StockQuantity,
-        int CategoryId
-    );
+     int Id,
+     string Name,
+     string Description,
+     decimal Price,
+     int StockQuantity,
+     int CategoryId,
+     CategoryDto Category
+ );
+
 }
