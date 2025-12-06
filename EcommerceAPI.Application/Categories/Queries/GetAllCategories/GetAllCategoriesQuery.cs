514365eec6f7f8d62d0c0c03769e88cf1fba668a
shakana0 <shakanajamal3@gmail.com>
2025-12-06 13:56:02 +0100

    feat: add GetAllCategoriesQuery

---
+++ EcommerceAPI.Application/Categories/Queries/GetAllCategories/GetAllCategoriesQuery.cs
@@ -0,0 +1,9 @@
+﻿using EcommerceAPI.Application.Categories.Dtos;
+using MediatR;
+
+namespace EcommerceAPI.Application.Categories.Queries.GetAllCategories
+{
+    public class GetAllCategoriesQuery : IRequest<IEnumerable<CategoryDto>>
+    {
+    }
+}
