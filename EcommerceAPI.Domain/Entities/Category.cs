7a1bfa217017cbad016ccae0fb50fcce1b79b46e
shakana0 <shakanajamal3@gmail.com>
2025-12-06 15:02:38 +0100

    refactor: update entity to implement excapsulation

--- EcommerceAPI.Domain/Entities/Category.cs
+++ EcommerceAPI.Domain/Entities/Category.cs
@@ -4,19 +4,29 @@ namespace EcommerceAPI.Domain.Entities;
 
 public class Category
 {
-    public int Id {get; set;}
+    public int Id { get; private set; }
+
     [Required]
     [MaxLength(100)]
-    public string Name { get; set; } = string.Empty;
+    public string Name { get; private set; } = string.Empty;
 
     [MaxLength(500)]
-    public string Description { get; set; } = string.Empty;
+    public string Description { get; private set; } = string.Empty;
 
     public Category() { }
 
-    public Category(string name, string description, int? parentCategoryId = null)
+    public Category(string name, string description)
     {
         Name = name;
         Description = description;
+    }
+
+    public void UpdateDetails(string name, string description)
+    {
+        if (!string.IsNullOrWhiteSpace(name))
+            Name = name;
+
+        if (!string.IsNullOrWhiteSpace(description))
+            Description = description;
     }
 }
