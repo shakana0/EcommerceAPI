5422dd01346a57f706a31f87855598ea75ea5c50
shakana0 <shakanajamal3@gmail.com>
2025-12-13 11:27:22 +0100

    feat: add method definition for  GetPagedAsync and CountAsync

--- Optimized/EcommerceAPI.Domain/Interfaces/IProductRepository.cs
+++ Optimized/EcommerceAPI.Domain/Interfaces/IProductRepository.cs
@@ -6,12 +6,10 @@ namespace EcommerceAPI.Domain.Interfaces
     public interface IProductRepository
     {
         Task<Product> AddAsync(Product product, CancellationToken cancellationToken);
-
+        Task<IEnumerable<Product>> GetPagedAsync(int page, int pageSize, CancellationToken cancellationToken);
+        Task<int> CountAsync(CancellationToken cancellationToken);
         Task<Product?> GetByIdAsync(int id, CancellationToken cancellationToken);
-        Task<IEnumerable<Product>> GetAllAsync(CancellationToken cancellationToken);
-
         Task<Product?> UpdateAsync(int id, string name, string description, decimal price, int stockQuantity, int categoryId, CancellationToken cancellationToken);
-
         Task<bool> DeleteAsync(int id, CancellationToken cancellationToken);
     }
 }
