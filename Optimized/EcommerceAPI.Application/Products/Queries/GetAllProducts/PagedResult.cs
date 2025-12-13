af3d7829d69b2dea04e0f4c5ff50823fceacaecd
shakana0 <shakanajamal3@gmail.com>
2025-12-13 11:50:28 +0100

    feat: add PagedResult class

---
+++ Optimized/EcommerceAPI.Application/Products/Queries/GetAllProducts/PagedResult.cs
@@ -0,0 +1,8 @@
+public class PagedResult<T>
+{
+    public IEnumerable<T> Items { get; set; } = new List<T>();
+    public int TotalCount { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+}
